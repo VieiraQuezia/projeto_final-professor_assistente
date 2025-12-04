2a017f15e0a780cfc1da4fc65aff2e3a540a0923
Guih <ggsantana18@gmail.com>
2025-12-04 07:16:51 -0300

    att expectativas

--- src/components/expectativasData.jsx
+++ src/components/expectativasData.jsx
@@ -1,50 +1,100 @@
-// src/data/expectativasData.js
-
 // ------------------------
 // HABILIDADES 6º ao 9º
 // ------------------------
 export const habilidades6a9 = [
-  { numeroHabilidade: "EF6a9.LI.1.01", descricao: "Organizar tópicos para produção de texto oral ou escrito considerando finalidade comunicativa.", palavraChave: "produção" },
-  { numeroHabilidade: "EF6a9.LI.2.08", descricao: "Identificar o assunto global de textos orais e escritos.", palavraChave: "assunto" },
-  { numeroHabilidade: "EF6a9.LI.2.09", descricao: "Reconhecer características e função comunicativa de textos orais, impressos e digitais.", palavraChave: "características" },
-  { numeroHabilidade: "EF6a9.LI.2.10", descricao: "Localizar informações explícitas e inferir informações implícitas em textos orais e escritos.", palavraChave: "informações" },
-  { numeroHabilidade: "EF6a9.LI.2.11", descricao: "Apreciar e fruir textos literários em língua inglesa.", palavraChave: "literários" },
+  { numeroHabilidade: "EF6a9.LI.4.36", descricao: "Comparar culturas locais com culturas de países anglófonos.", palavraChave: "culturas" },
+  { numeroHabilidade: "EF6a9.LI.5.41", descricao: "Reconhecer polissemia em itens lexicais.", palavraChave: "polissemia" },
+  { numeroHabilidade: "EF6a9.LI.6.63", descricao: "Realizar curadoria de fontes confiáveis em ambientes virtuais em inglês.", palavraChave: "curadoria" },
+  { numeroHabilidade: "EF6a9.LI.4.38", descricao: "Explorar variações da língua inglesa e evitar preconceitos linguísticos.", palavraChave: "variações" },
   { numeroHabilidade: "EF6a9.LI.2.12", descricao: "Desenvolver leitura de si e dos outros por meio de textos diversos.", palavraChave: "leitura" },
-  { numeroHabilidade: "EF6a9.LI.3.17", descricao: "Utilizar a língua inglesa de modo inteligível em situações comunicativas.", palavraChave: "utilizar" },
-  { numeroHabilidade: "EF6a9.LI.3.18", descricao: "Solicitar esclarecimentos sobre palavras e expressões desconhecidas.", palavraChave: "esclarecimentos" },
-  { numeroHabilidade: "EF6a9.LI.3.19", descricao: "Falar de si e de sua cultura a partir da língua inglesa.", palavraChave: "cultura" },
   { numeroHabilidade: "EF6a9.LI.4.33", descricao: "Reconhecer aspectos culturais, sociais e linguísticos em diferentes contextos.", palavraChave: "culturais" },
+  { numeroHabilidade: "EF6a9.LI.3.17", descricao: "Utilizar a língua inglesa de modo inteligível em situações comunicativas.", palavraChave: "utilizar" },
+  { numeroHabilidade: "EF6a9.LI.6.60", descricao: "Desenvolver pronúncia com auxílio de ferramentas tecnológicas.", palavraChave: "pronúncia" },
+  { numeroHabilidade: "EF6a9.LI.1.01", descricao: "Organizar tópicos para produção de texto oral ou escrito considerando finalidade comunicativa.", palavraChave: "produção" },
+  { numeroHabilidade: "EF6a9.LI.4.37", descricao: "Identificar a presença da língua inglesa no contexto mundial e globalização.", palavraChave: "globalização" },
+  { numeroHabilidade: "EF6a9.LI.5.42", descricao: "Identificar traços culturais em itens lexicais comparando com o português.", palavraChave: "traços" },
+  { numeroHabilidade: "EF6a9.LI.3.18", descricao: "Solicitar esclarecimentos sobre palavras e expressões desconhecidas.", palavraChave: "esclarecimentos" },
+  { numeroHabilidade: "EF6a9.LI.4.39", descricao: "Analisar influência de aspectos culturais e linguísticos sobre expressões e comportamentos.", palavraChave: "comportamentos" },
+  { numeroHabilidade: "EF6a9.LI.2.11", descricao: "Apreciar e fruir textos literários em língua inglesa.", palavraChave: "literários" },
+  { numeroHabilidade: "EF6a9.LI.6.61", descricao: "Conhecer patrimônios histórico-literários de países de língua inglesa.", palavraChave: "patrimônios" },
+  { numeroHabilidade: "EF6a9.LI.3.19", descricao: "Falar de si e de sua cultura a partir da língua inglesa.", palavraChave: "cultura" },
+  { numeroHabilidade: "EF6a9.LI.6.62", descricao: "Utilizar ferramentas tecnológicas para ampliar repertório linguístico-discursivo.", palavraChave: "tecnologia" },
   { numeroHabilidade: "EF6a9.LI.4.34", descricao: "Mapear países de língua inglesa e relacionar ao processo de colonização.", palavraChave: "países" },
   { numeroHabilidade: "EF6a9.LI.4.35", descricao: "Conhecer manifestações artísticas produzidas em língua inglesa.", palavraChave: "manifestações" },
-  { numeroHabilidade: "EF6a9.LI.4.36", descricao: "Comparar culturas locais com culturas de países anglófonos.", palavraChave: "culturas" },
-  { numeroHabilidade: "EF6a9.LI.4.37", descricao: "Identificar a presença da língua inglesa no contexto mundial e globalização.", palavraChave: "globalização" },
-  { numeroHabilidade: "EF6a9.LI.4.38", descricao: "Explorar variações da língua inglesa e evitar preconceitos linguísticos.", palavraChave: "variações" },
-  { numeroHabilidade: "EF6a9.LI.4.39", descricao: "Analisar influência de aspectos culturais e linguísticos sobre expressões e comportamentos.", palavraChave: "comportamentos" },
-  { numeroHabilidade: "EF6a9.LI.5.40", descricao: "Analisar aspectos fonéticos e fonológicos e diferentes sotaques.", palavraChave: "sotaques" },
-  { numeroHabilidade: "EF6a9.LI.5.41", descricao: "Reconhecer polissemia em itens lexicais.", palavraChave: "polissemia" },
-  { numeroHabilidade: "EF6a9.LI.5.42", descricao: "Identificar traços culturais em itens lexicais comparando com o português.", palavraChave: "traços" },
-  { numeroHabilidade: "EF6a9.LI.6.60", descricao: "Desenvolver pronúncia com auxílio de ferramentas tecnológicas.", palavraChave: "pronúncia" },
-  { numeroHabilidade: "EF6a9.LI.6.61", descricao: "Conhecer patrimônios histórico-literários de países de língua inglesa.", palavraChave: "patrimônios" },
-  { numeroHabilidade: "EF6a9.LI.6.62", descricao: "Utilizar ferramentas tecnológicas para ampliar repertório linguístico-discursivo.", palavraChave: "tecnologia" },
-  { numeroHabilidade: "EF6a9.LI.6.63", descricao: "Realizar curadoria de fontes confiáveis em ambientes virtuais em inglês.", palavraChave: "curadoria" },
-  { numeroHabilidade: "EF6a9.LI.6.64", descricao: "Praticar condutas e linguagens éticas em contextos digitais.", palavraChave: "éticas" }
+  { numeroHabilidade: "EF6a9.LI.2.09", descricao: "Reconhecer características e função comunicativa de textos orais, impressos e digitais.", palavraChave: "características" },
+  { numeroHabilidade: "EF6a9.LI.2.08", descricao: "Identificar o assunto global de textos orais e escritos.", palavraChave: "assunto" },
+  { numeroHabilidade: "EF6a9.LI.2.10", descricao: "Localizar informações explícitas e inferir informações implícitas em textos orais e escritos.", palavraChave: "informações" },
+  { numeroHabilidade: "EF6a9.LI.6.64", descricao: "Praticar condutas e linguagens éticas em contextos digitais.", palavraChave: "éticas" },
+  { numeroHabilidade: "EF6a9.LI.5.40", descricao: "Analisar aspectos fonéticos e fonológicos e diferentes sotaques.", palavraChave: "sotaques" }
+];
+
+// ------------------------
+// HABILIDADES 6º ano
+// ------------------------
+export const habilidades6 = [
+  { numeroHabilidade: "EF06.LI.3.21", descricao: "Interagir em situações comunicativas ligadas à descrição do bairro, de tipos de moradia e da própria casa.", palavraChave: "moradia" },
+  { numeroHabilidade: "EF06.LI.5.43", descricao: "Apropriar-se de repertório lexical ligado às temáticas de Cidadania, Territorialidade e Lazer.", palavraChave: "léxico" },
+  { numeroHabilidade: "EF06.LI.5.48", descricao: "Analisar o uso de verbos existenciais, artigos indefinidos, formação de plural, caso genitivo e pronomes possessivos.", palavraChave: "existenciais" },
+  { numeroHabilidade: "EF06.LI.3.20", descricao: "Interagir em situações comunicativas ligadas à troca de informações pessoais e descrição de características físicas e de personalidade.", palavraChave: "interação" },
+  { numeroHabilidade: "EF06.LI.1.02", descricao: "Produzir textos em língua inglesa de diferentes gêneros textuais e em diferentes modos semióticos a partir das temáticas de Cidadania, Territorialidade e Lazer.", palavraChave: "gêneros" },
+  { numeroHabilidade: "EF06.LI.3.22", descricao: "Interagir em situações comunicativas ligadas à descrição de habilidades, atividades e a gostos pessoais.", palavraChave: "gostos" },
+  { numeroHabilidade: "EF06.LI.5.47", descricao: "Analisar o uso de verbos no presente simples, bem como pronomes pessoais e interrogativos.", palavraChave: "presente" },
+  { numeroHabilidade: "EF06.LI.2.13", descricao: "Localizar informações explícitas e inferir informações implícitas de textos ligados às temáticas de Cidadania, Territorialidade e Lazer.", palavraChave: "estratégias" },
+  { numeroHabilidade: "EF06.LI.5.49", descricao: "Analisar o uso de modal can, advérbios, pronomes demonstrativos e presente simples.", palavraChave: "can" }
+];
+
+// ------------------------
+// HABILIDADES 6º e 7º ano
+// ------------------------
+export const habilidades6e7 = [
+  { numeroHabilidade: "EF6e7.LI.1.06", descricao: "Revisar, com a mediação do(a) professor(a), produções autorais, considerando as características dos gêneros estudados.", palavraChave: "revisão" }
+];
+
+// ------------------------
+// HABILIDADES 7º ano
+// ------------------------
+export const habilidades7 = [
+  { numeroHabilidade: "EF07.LI.5.50", descricao: "Analisar o uso de presente progressivo, preposições, artigos definidos, verbos existenciais e pronomes interrogativos.", palavraChave: "progressivo" },
+  { numeroHabilidade: "EF07.LI.3.24", descricao: "Interagir em situações comunicativas ligadas a sugestões e recomendações, bem como a hábitos, rotinas, tarefas e sentimentos.", palavraChave: "sugestões" },
+  { numeroHabilidade: "EF07.LI.2.14", descricao: "Localizar informações explícitas e inferir informações implícitas de textos ligados às temáticas de Sustentabilidade, Consciência Social e Entretenimento.", palavraChave: "inferência" },
+  { numeroHabilidade: "EF07.LI.5.51", descricao: "Analisar o uso de modal should, passado simples e conjunções coordenativas.", palavraChave: "should" },
+  { numeroHabilidade: "EF07.LI.3.25", descricao: "Interagir em situações comunicativas ligadas a preferências de leitura, festas e viagens.", palavraChave: "preferências" },
+  { numeroHabilidade: "EF07.LI.5.44", descricao: "Apropriar-se de repertório lexical ligado às temáticas de Sustentabilidade, Consciência Social e Entretenimento.", palavraChave: "vocabulário" },
+  { numeroHabilidade: "EF07.LI.1.03", descricao: "Produzir textos em língua inglesa de diferentes gêneros textuais e em diferentes modos semióticos a partir das temáticas de Sustentabilidade, Consciência Social e Entretenimento.", palavraChave: "produção" },
+  { numeroHabilidade: "EF07.LI.3.23", descricao: "Interagir em situações comunicativas ligadas à obtenção de informações para a mobilidade (localizações e direções).", palavraChave: "mobilidade" },
+  { numeroHabilidade: "EF07.LI.5.52", descricao: "Analisar o uso de marcadores temporais, advérbios, preposições, modal would e phrasal verbs.", palavraChave: "marcadores" }
+];
+
+// ------------------------
+// HABILIDADES 8º ano
+// ------------------------
+export const habilidades8 = [
+  { numeroHabilidade: "EF08.LI.3.27", descricao: "Interagir em situações comunicativas ligadas a fatos históricos, políticos e culturais e respeito às diferenças.", palavraChave: "sociedade" },
+  { numeroHabilidade: "EF08.LI.5.55", descricao: "Analisar o uso de comparativos e superlativos, formas verbais de used to, advérbios e futuro simples.", palavraChave: "comparativos" },
+  { numeroHabilidade: "EF08.LI.1.04", descricao: "Produzir textos em língua inglesa de diferentes gêneros textuais a partir das temáticas de Saúde e Estilo de Vida, História e Sociedade, e Arte e Cultura.", palavraChave: "temáticas" },
+  { numeroHabilidade: "EF08.LI.3.26", descricao: "Interagir em situações comunicativas ligadas a visitas ao médico, práticas de exercícios físicos e alimentação.", palavraChave: "saúde" },
+  { numeroHabilidade: "EF08.LI.5.45", descricao: "Apropriar-se de repertório lexical ligado às temáticas de Saúde e Estilo de Vida, História e Sociedade, e Arte e Cultura.", palavraChave: "dicionários" },
+  { numeroHabilidade: "EF08.LI.2.15", descricao: "Localizar informações explícitas e inferir informações implícitas de textos ligados às temáticas de Saúde e Estilo de Vida, História e Sociedade, e Arte e Cultura.", palavraChave: "leitura" },
+  { numeroHabilidade: "EF08.LI.3.28", descricao: "Compartilhar gostos e preferências ligadas às diferentes manifestações artístico-culturais.", palavraChave: "arte" },
+  { numeroHabilidade: "EF08.LI.5.54", descricao: "Analisar o uso de modais would, must, can, could e may, bem como de pronomes relativos.", palavraChave: "modais" },
+  { numeroHabilidade: "EF08.LI.5.53", descricao: "Analisar o uso de imperativo, marcadores sequenciais, conjunções correlativas, collocations e quantificadores.", palavraChave: "imperativo" }
 ];
 
 // ------------------------
 // HABILIDADES 9º ano
 // ------------------------
 export const habilidades9 = [
-  { numeroHabilidade: "EF09.LI.1.05", descricao: "Produzir textos em inglês de diferentes gêneros e modos semióticos.", palavraChave: "produzir" },
-  { numeroHabilidade: "EF09.LI.2.16", descricao: "Localizar informações explícitas e implícitas em textos de temáticas específicas.", palavraChave: "informações" },
-  { numeroHabilidade: "EF09.LI.3.29", descricao: "Apresentar oralmente resultados de pesquisa em inglês.", palavraChave: "apresentar" },
-  { numeroHabilidade: "EF09.LI.3.30", descricao: "Interagir em situações comunicativas ligadas ao Mundo do Trabalho.", palavraChave: "comunicação" },
-  { numeroHabilidade: "EF09.LI.3.31", descricao: "Posicionar-se com criticidade e ética em produções comunicativas.", palavraChave: "ética" },
+  { numeroHabilidade: "EF09.LI.5.59", descricao: "Comparar verbos no passado, presente e futuro.", palavraChave: "tempos" },
   { numeroHabilidade: "EF09.LI.3.32", descricao: "Compartilhar sentimentos e planos futuros.", palavraChave: "sentimentos" },
-  { numeroHabilidade: "EF09.LI.5.46", descricao: "Apropriar-se de repertório lexical das áreas do Mundo do Trabalho e Ciência e Tecnologia.", palavraChave: "lexical" },
-  { numeroHabilidade: "EF09.LI.5.56", descricao: "Analisar uso de modais, quantificadores e tempos verbais.", palavraChave: "modais" },
   { numeroHabilidade: "EF09.LI.5.57", descricao: "Analisar uso do imperativo e conjunções.", palavraChave: "imperativo" },
+  { numeroHabilidade: "EF09.LI.5.46", descricao: "Apropriar-se de repertório lexical das áreas do Mundo do Trabalho e Ciência e Tecnologia.", palavraChave: "lexical" },
+  { numeroHabilidade: "EF09.LI.1.05", descricao: "Produzir textos em inglês de diferentes gêneros e modos semióticos.", palavraChave: "produzir" },
+  { numeroHabilidade: "EF09.LI.3.30", descricao: "Interagir em situações comunicativas ligadas ao Mundo do Trabalho.", palavraChave: "comunicação" },
+  { numeroHabilidade: "EF09.LI.5.56", descricao: "Analisar uso de modais, quantificadores e tempos verbais.", palavraChave: "modais" },
+  { numeroHabilidade: "EF09.LI.3.29", descricao: "Apresentar oralmente resultados de pesquisa em inglês.", palavraChave: "apresentar" },
+  { numeroHabilidade: "EF09.LI.2.16", descricao: "Localizar informações explícitas e implícitas em textos de temáticas específicas.", palavraChave: "informações" },
   { numeroHabilidade: "EF09.LI.5.58", descricao: "Analisar 'used to', condicionais e pronomes relativos.", palavraChave: "used to" },
-  { numeroHabilidade: "EF09.LI.5.59", descricao: "Comparar verbos no passado, presente e futuro.", palavraChave: "tempos" }
+  { numeroHabilidade: "EF09.LI.3.31", descricao: "Posicionar-se com criticidade e ética em produções comunicativas.", palavraChave: "ética" }
 ];
 
 // ------------------------
