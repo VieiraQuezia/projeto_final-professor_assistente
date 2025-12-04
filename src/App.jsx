4e092778722ca7150e912d2bc68bca47f9e68763
Guih <ggsantana18@gmail.com>
2025-12-04 07:37:43 -0300

    att vercel error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 import Home from './pages/Home';
 import Expectativas from './pages/Expectativas';
 import Exercicios from './pages/Exercicios';
